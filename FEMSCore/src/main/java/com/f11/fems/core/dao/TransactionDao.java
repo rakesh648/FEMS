c116ec6e62f88097f141894a7a7bf6faab6292b7
Rakesh <rakesh648@gmail.com>
2017-06-21 00:12:57 +0530

    a

--- FEMSCore/src/main/java/com/f11/fems/core/dao/TransactionDao.java
+++ FEMSCore/src/main/java/com/f11/fems/core/dao/TransactionDao.java
@@ -7,10 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.f11.fems.core.entity.Fund;
 import com.f11.fems.core.entity.Transaction;
-import com.f11.fems.core.entity.type.TransactionGroup;
 
 public interface TransactionDao extends JpaRepository<Transaction, Long>{
-	Collection<Transaction> findByTransactionGroup(TransactionGroup group);
 	Collection<Transaction> findByFund(Fund fund);	
 	Optional<Transaction> findById(Long id);
 }
